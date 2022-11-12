ace97eb50e2baab33edd94e254b81ed2478c864e
Sarosh Farhan <saroshfarhan6495@gmail.com>
2022-11-12 18:57:45 +0530

    added delete functionality and local storage

--- src/App.js
+++ src/App.js
@@ -5,18 +5,39 @@ import logo from './assets/favicon-32x32.png';
 import Todo from './components/Todo';
 
 function App() {
-  const [todo, setTodo] = React.useState([]);
-
+  const [task, setTask] = React.useState("")
+  const [todo, setTodo] = React.useState(() => JSON.parse(localStorage.getItem('tasks')) || []);
+  const inputRef = React.useRef(null)
   
   function addTodo(){
-    let task = {
+    let taskObj = {
       isCompleted: false,
-      text: "",
+      task: task,
       id: nanoid()
     }
-    setTodo(prevTodo => [task, ...prevTodo])
+    //this sets tasks in react state
+    setTodo(prevTodo => [taskObj, ...prevTodo])
+    saveTodo([taskObj, ...todo])
+    //this clears the input field once we have aded a task
+    inputRef.current.value = ""
+    //thist clears the task text so that user cannot add empty task
+    setTask("")
   }
 
+  function saveTodo(todo){
+    localStorage.setItem('tasks', JSON.stringify(todo));
+  }
+
+  function handleChange(event){
+    setTask(prevTask =>  prevTask = event.target.value)
+  }
+  
+  function onDelete(index){
+    var clonedTasks = [...todo];
+		clonedTasks.splice(index, 1);
+		setTodo(clonedTasks);
+    saveTodo(clonedTasks)
+  }
 
 
   return (
@@ -25,9 +46,22 @@ function App() {
         <img src={logo} alt='logo' className='mx-2 my-1 w-8 h-8'></img>
         <h1 className="text-4xl font-bold text-center text-amber-700">TODO APP</h1>
       </div>
-      <div className='px-4 my-8 lg:px-48'>
-        <div className='flex justify-end'>
-          <button type='button' onClick={addTodo} className='w-full lg:w-auto md:w-auto bg-amber-700 hover:bg-amber-800 text-white font-bold py-2 px-4 rounded inline-flex items-center justify-center'>
+      <div className='px-4 my-8 lg:px-48'> 
+        <div className='flex justify-between'>
+          <input 
+            type='text' 
+            className='border rounded hover:border-amber-600 focus:outline-none pl-4 focus:border-amber-600 w-[14rem] lg:w-[46rem] md:w-[36rem] flex justify-start' 
+            placeholder='Add task here'
+            name='task'
+            onChange={handleChange}
+            ref={inputRef}
+          />
+          <button 
+            type='button' 
+            onClick={addTodo} 
+            disabled={!task}
+            className='disabled:cursor-not-allowed disabled:opacity-50 lg:w-auto md:w-auto bg-amber-700 hover:bg-amber-800 text-white font-bold py-2 px-4 rounded inline-flex items-center justify-center'
+            >
             <svg className="fill-current w-6 h-6 mr-2 svg-icon" viewBox="0 0 20 20">
                   <path d="M14.613,10c0,0.23-0.188,0.419-0.419,0.419H10.42v3.774c0,0.23-0.189,0.42-0.42,0.42s-0.419-0.189-0.419-0.42v-3.774H5.806c-0.23,0-0.419-0.189-0.419-0.419s0.189-0.419,0.419-0.419h3.775V5.806c0-0.23,0.189-0.419,0.419-0.419s0.42,0.189,0.42,0.419v3.775h3.774C14.425,9.581,14.613,9.77,14.613,10 M17.969,10c0,4.401-3.567,7.969-7.969,7.969c-4.402,0-7.969-3.567-7.969-7.969c0-4.402,3.567-7.969,7.969-7.969C14.401,2.031,17.969,5.598,17.969,10 M17.13,10c0-3.932-3.198-7.13-7.13-7.13S2.87,6.068,2.87,10c0,3.933,3.198,7.13,7.13,7.13S17.13,13.933,17.13,10"></path>
             </svg>
@@ -38,7 +72,7 @@ function App() {
         { 
           todo.length === 0 ? <h1 className='text-2xl font-semibold text-center text-[#69DC9E]'>All tasks accomplished!!</h1>
           :
-          <Todo data={todo}/>
+          <Todo data={todo} onDelete={onDelete}/>
         }
       </div>
       
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,63 +2,23 @@ import React from 'react';
 
 export default function Todo(props) {
 
-    const [value, setValue] = React.useState();
-    const [editingValue, setEditingValue] = React.useState(value);
-
-    const onChange = (event) => {
-        setEditingValue(event.target.value);
-    }
-
-    const onKeyDown = (event) => {
-        if (event.key === "Enter" || event.key === "Escape") {
-        event.target.blur();
-        }
-    };
-
-    const onBlur = (event) => {
-        if (event.target.value.trim() === "") {
-        setEditingValue(value);
-        } else {
-        setValue(event.target.value);
-        }
-    };
-
-    const onInput = (target) => {
-        if (target.scrollHeight > 33) {
-        target.style.height = "24px";
-        target.style.height = target.scrollHeight - 8 + "px";
-        }
-    };
-
-    // const textareaRef = React.useRef();
-
-    // React.useEffect(() => {
-    //     onInput(textareaRef.current);
-    // }, [onInput, textareaRef]);
- 
-
-   
-   const tasks = props.data.map((task) => {
-
+   const tasks = props.data.map((task, index) => {
     return (
         <div className='rounded overflow-hidden shadow-md my-1' key={task.id}>
-            <div className='flex items-center text-center'>
-                <input type="checkbox" name="" id="" className='default:ring-2 mx-4 lg:mx-4 '/>
-                <div className='w-auto my-4 pr-4 lg:px-2 lg:pr-0'>
-                    <textarea
-                        className='resize-none overflow-hidden min-h-[24px] max-h-[100px] hover:cursor-pointer text-base text-left mr-24 lg:w-[36rem] lg:mr-0 md:mr-[28rem]'
-                        rows={1}
-                        aria-label="Todo"
-                        value={editingValue}
-                        onBlur={onBlur}
-                        onChange={onChange}
-                        onKeyDown={onKeyDown}
-                        onInput={(event) => onInput(event.target)}
-                        // ref={textareaRef}
-                        placeholder="e.g Grocery shopping"
-                    />
+            <div className='flex items-center text-center justify-between'>
+                {/* <input 
+                    type="checkbox" 
+                    name="isCompleted" 
+                    id="isCompleted" 
+                    className='default:ring-2 mx-4 lg:mx-4'
+                /> for future purposes*/}
+                <div className='w-auto my-4 ml-2 pr-4 lg:px-2 lg:pr-0'>
+                    <p className='px-2 lg:px2 w-auto text-left'>{task.task}</p>
                 </div>
-                <div className='items-end ml-0 mr-4 lg:ml-[14rem] hover:text-red-500 hover:cursor-pointer'>
+                <div 
+                    className='items-end ml-0 mr-4 lg:ml-[14rem] hover:text-red-500 hover:cursor-pointer'
+                    onClick={() => props.onDelete(index)}
+                    >
                     <svg className="w-5 h-5 mb-1" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
                         <path d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                     </svg>
@@ -66,13 +26,13 @@ export default function Todo(props) {
             </div>
         </div>
     )
-   }
+}
    ) 
              
 
     return (
         <>
-            {tasks}
+          {tasks}
         </>
     )
 }
